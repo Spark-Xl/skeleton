5cb00c8485595af41ae48c1d30075f776e83de3f
Spark-Xl <sparkshenxl@gmail.com>
2017-09-04 17:38:04 -0400

    [0904] create controller and dao for Tag

---
+++ src/main/java/dao/TagDao.java
@@ -0,0 +1,44 @@
+package dao;
+
+import generated.tables.records.TagsRecord;
+import generated.tables.records.ReceiptsRecord;
+
+import static generated.Tables.RECEIPTS;
+import static generated.Tables.TAGS;
+
+import org.jooq.Configuration;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.impl.DSL;
+import org.jooq.Result;
+
+import java.util.List;
+
+
+public class TagDao {
+    DSLContext dsl;
+
+    public TagDao(Configuration jooqConfig) {
+        this.dsl = DSL.using(jooqConfig);
+    }
+
+    public void updateTag(Integer id, String tagname) {
+        List<TagsRecord> tagRecords = dsl.selectFrom(TAGS).where(TAGS.ID.eq(id)).and(TAGS.TAG.eq(tagname)).fetch();
+
+        System.out.println(tagRecords);
+        if (tagRecords.size() == 0) {
+            dsl.insertInto(TAGS, TAGS.ID, TAGS.TAG).values(id, tagname).execute();
+        } else {
+            dsl.deleteFrom(TAGS).where(TAGS.ID.eq(id)).and(TAGS.TAG.eq(tagname)).execute();
+        }
+
+        tagRecords = dsl.selectFrom(TAGS).fetch();
+        System.out.println(tagRecords);
+    }
+
+    public List<ReceiptsRecord> getReceiptsByTag(String tagname) {
+        List<TagsRecord> tagRecords = dsl.selectFrom(TAGS).where(TAGS.TAG.eq(tagname)).fetch();
+        System.out.print(tagRecords);
+        return dsl.selectFrom(RECEIPTS).where(RECEIPTS.ID.in(dsl.selectFrom(TAGS).where(TAGS.TAG.eq(tagname)).fetch().getValues(TAGS.ID))).fetch();
+    }
+}
