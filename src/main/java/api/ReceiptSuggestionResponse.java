9553d6dcdb2e074c87b9fb584a2c808017de3173
Spark-Xl <sparkshenxl@gmail.com>
2017-10-05 22:06:05 -0400

    get name, amount, overall boundary

--- src/main/java/api/ReceiptSuggestionResponse.java
+++ src/main/java/api/ReceiptSuggestionResponse.java
@@ -1,5 +1,6 @@
 package api;
 
+import com.google.cloud.vision.v1.*;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.math.BigDecimal;
 
@@ -13,9 +14,32 @@ public class ReceiptSuggestionResponse {
     @JsonProperty
     public final BigDecimal amount;
 
+    @JsonProperty
+    public Integer left_x;
+
+	@JsonProperty
+    public Integer right_x;
+
+    @JsonProperty
+    public Integer top_y;
+
+    @JsonProperty
+    public Integer bottom_y;
 
     public ReceiptSuggestionResponse(String merchantName, BigDecimal amount) {
         this.merchantName = merchantName;
         this.amount = amount;
+
+        this.left_x = null;
+		this.right_x = null;
+		this.top_y = null;
+		this.bottom_y = null;
+    }
+
+    public void setBoundrary(Integer left_x, Integer right_x, Integer top_y, Integer bottom_y) {
+        this.left_x = left_x;
+		this.right_x = right_x;
+		this.top_y = top_y;
+		this.bottom_y = bottom_y;
     }
 }
--- src/main/java/controllers/ReceiptImageController.java
+++ src/main/java/controllers/ReceiptImageController.java
@@ -9,6 +9,9 @@ import java.util.Collections;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import org.hibernate.validator.constraints.NotEmpty;
+import com.google.cloud.vision.v1.Vertex;
+import com.google.cloud.vision.v1.BoundingPoly;
+import java.util.List;
 
 import static java.lang.System.out;
 
@@ -48,16 +51,74 @@ public class ReceiptImageController {
             String merchantName = null;
             BigDecimal amount = null;
 
+            Integer left_x = null;
+            Integer right_x = null;
+            Integer top_y = null;
+            Integer bottom_y = null;
+
             // Your Algo Here!!
             // Sort text annotations by bounding polygon.  Top-most non-decimal text is the merchant
             // bottom-most decimal text is the total amount
-            for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
-                out.printf("Position : %s\n", annotation.getBoundingPoly());
-                out.printf("Text: %s\n", annotation.getDescription());
+            for (int i = 0; i < res.getTextAnnotationsList().size(); i ++) {
+                EntityAnnotation annotation = res.getTextAnnotationsList().get(i);
+                String text = annotation.getDescription();
+                // save the full text bounding
+
+                BoundingPoly bp = annotation.getBoundingPoly();
+                if (i == 0) {
+                    for (int j = 0; j < 4; j++) {
+                        Vertex v= bp.getVertices(j);
+                        Integer x = v.getX();
+                        Integer y = v.getY();
+
+                        if (left_x == null) {
+                            left_x = x;
+                            right_x = x;
+                        } else {
+                            left_x = new Integer(Math.min(x, left_x));
+                            right_x = new Integer(Math.max(x, right_x));
+                        }
+
+                        if (top_y == null) {
+                            top_y = y;
+                            bottom_y = y;
+                        } else {
+                            top_y = new Integer(Math.min(top_y, y));
+                            bottom_y = new Integer(Math.max(bottom_y, y));
+                        }
+                    }
+                    continue;
+                }
+
+                if (getDecimal(text) == null) {
+                    merchantName = text;
+                    break;
+                }
+            }
+
+            for (int i = res.getTextAnnotationsList().size() - 1; i >= 0; i --) {
+                String text = res.getTextAnnotationsList().get(i).getDescription();
+                amount = getDecimal(text);
+                if (amount != null)
+                    break;
             }
 
             //TextAnnotation fullTextAnnotation = res.getFullTextAnnotation();
-            return new ReceiptSuggestionResponse(merchantName, amount);
+            ReceiptSuggestionResponse response = new ReceiptSuggestionResponse(merchantName, amount);
+            response.setBoundrary(left_x, right_x, top_y, bottom_y);
+
+            return response;
         }
+    }
+
+    public BigDecimal getDecimal(@NotEmpty String s) {
+        try {
+            BigDecimal bd = new BigDecimal(s);
+            return bd;
+        }
+        catch(NumberFormatException e) {
+            return null;
+        }
+        // long value = bd.longValue();
     }
 }
